e4803089f1011dd67eb79294b531a0baf1550241
vladdos <1v1expert@gmail.com>
2018-01-09 00:18:29 +0300

    вв

--- get_if.py
+++ get_if.py
@@ -3,6 +3,7 @@ import datetime
 import json
 import time
 
+
 def get_time(param):
 	now = datetime.datetime.now()
 	str_time = str(now.strftime('%Y-%m-%d'))
@@ -11,6 +12,8 @@ def get_time(param):
 		return str_time
 	else:
 		return str_date
+
+
 def load_file_f():
 	puth = 'consignors.json'
 	puth_dumps = 'consignors_dumps.json'
@@ -32,11 +35,13 @@ def load_file_f():
 			filep.close()
 			return data21
 		except:
-			paxa = {}
+			paxa = dict()
 			with open(puth, 'w') as fiiil:
 				json.dump(paxa, fiiil, ensure_ascii=False)
 			fiiil.close()
 			return paxa
+
+
 def load_file_d():
 	puth = 'consignors_temp_' + str(get_time(True)) + 'json'
 	puth_dumps = 'consignors_dumps_temp_' + str(get_time(True)) + '.json'
@@ -63,6 +68,8 @@ def load_file_d():
 				json.dump(paxa, fiiil, ensure_ascii=False)
 			fiiil.close()
 			return paxa
+
+
 def rewritingg():
 	datings = load_file_f()
 	puth = 'consignors_temp_' + str(get_time(True)) + 'json'
@@ -73,37 +80,39 @@ def rewritingg():
 	with open(puth_dumps, 'w') as filet:
 		json.dump(datings, filet, ensure_ascii=False)
 	filet.close()
+
+
 def get_parcel_pulse():
 	conn = http.client.HTTPSConnection("api.pulse.express")
 	conn_2 = http.client.HTTPSConnection("api.pulse.express")
 	headers = {
-	'content-type': "application/json",
-	'authorization': ""
+		'content-type': "application/json",
+		'authorization': ""
 	}
-
+	
 	conn.request("GET", "/v1/parcels/?limit=300", headers=headers)
-
+	
 	res = conn.getresponse()
 	data = res.read()
 	conn.close()
 	datal = json.loads(data.decode("utf-8"))
 	keys = 0
-	while keys< 300:
+	while keys < 300:
 		parcelNums = None
 		print(datal['results'][keys]['order_id'], ' ', datal['results'][keys]['uid'])
 		order_id = datal['results'][keys]['order_id']
 		for y in datal['results'][keys]['barcodes']:
 			parcelNums = y
 			print('BARCODE = ', y)
-		#conn_2 = http.client.HTTPSConnection("api.pulse.express")
-		#q = "/v1/parcels/" + datal['results'][keys]['uid'] + "/events/"
+		# conn_2 = http.client.HTTPSConnection("api.pulse.express")
+		# q = "/v1/parcels/" + datal['results'][keys]['uid'] + "/events/"
 		conn_2.request("GET", "/v1/parcels/" + datal['results'][keys]['uid'] + "/events/", headers=headers)
-		#print(q)
+		# print(q)
 		reas = conn_2.getresponse()
 		dataz = reas.read()
-		#L = reas.read()
+		# L = reas.read()
 		L = json.loads(dataz.decode("utf-8"))
-		#print(L)
+		# print(L)
 		z = 0
 		issued_time = None
 		issued_date = None
@@ -112,42 +121,46 @@ def get_parcel_pulse():
 		delivering_time = None
 		delivering_date = None
 		for x in L:
-			#print(x)
-			#print(L[z]['data']['status'], '- status')
+			# print(x)
+			# print(L[z]['data']['status'], '- status')
 			time_edit = L[z]['datetime']
 			T_time = time_edit.find('T')
 			dot_time = time_edit.find('.')
-			#print(L[z]['datetime'][:T_time], 'date')
-			#print(L[z]['datetime'][T_time+1:dot_time], 'time')
+			# print(L[z]['datetime'][:T_time], 'date')
+			# print(L[z]['datetime'][T_time+1:dot_time], 'time')
 			try:
 				if L[z]['data']['status'] == 'Выдана':
-					print('посыль выдана ', L[z]['datetime'][:T_time], ' В ', L[z]['datetime'][T_time+1:dot_time])
-					issued_time = L[z]['datetime'][T_time+1:dot_time]
+					print('посыль выдана ', L[z]['datetime'][:T_time], ' В ', L[z]['datetime'][T_time + 1:dot_time])
+					issued_time = L[z]['datetime'][T_time + 1:dot_time]
 					issued_date = L[z]['datetime'][:T_time]
 				if L[z]['data']['status'] == 'Доставлена':
-					print('посыль доставлена ', L[z]['datetime'][:T_time], ' В ', L[z]['datetime'][T_time+1:dot_time])
-					delivered_time = L[z]['datetime'][T_time+1:dot_time]
+					print('посыль доставлена ', L[z]['datetime'][:T_time], ' В ', L[z]['datetime'][T_time + 1:dot_time])
+					delivered_time = L[z]['datetime'][T_time + 1:dot_time]
 					delivered_date = L[z]['datetime'][:T_time]
 				if L[z]['data']['status'] == 'В доставке':
-					print('посыль в доставке ', L[z]['datetime'][:T_time], ' В ', L[z]['datetime'][T_time+1:dot_time])
-					delivering_time = L[z]['datetime'][T_time+1:dot_time]		
+					print('посыль в доставке ', L[z]['datetime'][:T_time], ' В ', L[z]['datetime'][T_time + 1:dot_time])
+					delivering_time = L[z]['datetime'][T_time + 1:dot_time]
 					delivering_date = L[z]['datetime'][:T_time]
 			except:
-				print('succes')	
-
+				print('succes')
+			
 			z += 1
-		ref = 'https://internal.pulse.express/parcels/'+ datal['results'][keys]['uid']
-		add_dop(issued_time, issued_date, delivered_time, delivered_date, delivering_time, delivering_date, order_id, parcelNums, ref)
-		#print('da')
+		ref = 'https://internal.pulse.express/parcels/' + datal['results'][keys]['uid']
+		add_dop(issued_time, issued_date, delivered_time, delivered_date, delivering_time, delivering_date, order_id,
+		        parcelNums, ref)
+		# print('da')
 		keys += 1
-		#time.sleep(2)
-	#print(datal['results'])
-	#print(data.decode("utf-8"))
-def add_dop(issued_time, issued_date, delivered_time, delivered_date, delivering_time, delivering_date, order_id, parcelNum, ref):
+	# time.sleep(2)
+
+
+# print(datal['results'])
+# print(data.decode("utf-8"))
+def add_dop(issued_time, issued_date, delivered_time, delivered_date, delivering_time, delivering_date, order_id,
+            parcelNum, ref):
 	puth = 'consignors_temp_' + str(get_time(True)) + 'json'
 	puth_dumps = 'consignors_dumps_temp_' + str(get_time(True)) + '.json'
 	data_dop = load_file_d()
-	if issued_time == None: 
+	if issued_time == None:
 		issued_time = ' '
 		issued_date = ' '
 	if delivered_time == None:
@@ -155,11 +168,13 @@ def add_dop(issued_time, issued_date, delivered_time, delivered_date, delivering
 		delivered_date = ' '
 	if delivering_time == None:
 		delivering_time = ' '
-		delivering_date = ' ' 
-	print('issued_time = ', issued_time, ' issued_date = ', issued_date, ' delivered_time = ', delivered_time, 'delivered_date = ',delivered_date, ' delivering_time = ', delivering_time, ' delivering_date = ', delivering_date, ' order_id = ', order_id)
+		delivering_date = ' '
+	print('issued_time = ', issued_time, ' issued_date = ', issued_date, ' delivered_time = ', delivered_time,
+	      'delivered_date = ', delivered_date, ' delivering_time = ', delivering_time, ' delivering_date = ',
+	      delivering_date, ' order_id = ', order_id)
 	counts = data_dop['count']
 	i = 1
-	while i<counts+1:
+	while i < counts + 1:
 		if ((data_dop[str(i)]['dpd_order'] == order_id) and (data_dop[str(i)]['parcelNum:'] == parcelNum)):
 			data_dop[str(i)].update([('issued_time', issued_time)])
 			data_dop[str(i)].update([('issued_date', issued_date)])
@@ -169,7 +184,7 @@ def add_dop(issued_time, issued_date, delivered_time, delivered_date, delivering
 			data_dop[str(i)].update([('delivering_date', delivering_date)])
 			data_dop[str(i)].update([('Date', delivering_date)])
 			data_dop[str(i)].update([('ref', ref)])
-		i +=1
+		i += 1
 	with open(puth, 'w') as fileq:
 		json.dump(data_dop, fileq, ensure_ascii=False)
 		fileq.close()
