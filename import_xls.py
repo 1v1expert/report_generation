a6bd1f7134657c5f72ff519abfc893ab029fe6aa
V L A D D O S <1v1expert@gmail.com>
2018-01-09 13:24:04 +0300

    add send message module + compile

--- import_xls.py
+++ import_xls.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import json
 from openpyxl import Workbook, load_workbook
 import datetime
--- send_message.py
+++ send_message.py
@@ -1,7 +1,39 @@
-import conf
-import get_point_list
+# -*- coding: utf-8 -*-
 import smtplib
-from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 
+import conf
+
+you = 'To: ' + ', '.join(conf.toaddr)
+msg = MIMEMultipart('mixed')
+msg['Subject'] = 'Find consignors'
+msg['From'] = conf.me
+#msg['To'] = ', '.join(conf.toaddr[0:4])
+#msg['cc'] = ', '.join([ conf.toaddr[4] ])
+msg['To'] = ', '.join(conf.test_toaddr[0:2])
+# Genering E-mail
+puth = 'report.xlsx'
+try:
+	fo = open(puth, 'rb')
+	filecontent = fo.read()
+	fo.close()
+	part2 = MIMEApplication(filecontent, 'application/xls;name="report.xlsx"')
+
+except:
+	print('good')
+
+msg.attach(conf.part1_failure)
+#msg.attach(part2)
+msg.attach(conf.part3)
+
+# Connecting
+s = smtplib.SMTP(conf.server, conf.port)
+s.ehlo()
+s.starttls()
+s.ehlo()
+# Autorization
+s.login(conf.user_name, conf.user_passwd)
+# Send message
+s.sendmail(conf.me, conf.toaddr, msg.as_string())
+s.quit()
