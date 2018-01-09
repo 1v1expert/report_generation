0b279dd4586364728799362b077ca0840def2c97
V L A D D O S <1v1expert@gmail.com>
2018-01-09 13:01:03 +0300

    add send message module

---
+++ send_message.py
@@ -0,0 +1,7 @@
+import conf
+import get_point_list
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+
